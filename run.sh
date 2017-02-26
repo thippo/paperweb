ea6277222108891a5c40e22dd31202f12a959fcc
thippo <thippo@163.com>
2017-02-26 22:46:30 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+systemctl start iptables.service
+sleep 2
+systemctl stop iptables.service
+nohup /opt/mongodb-linux-x86_64-rhel70-3.2.8/bin/mongod --dbpath=/root/data/db &
+source /opt/anaconda3/bin/activate flaskpy35
